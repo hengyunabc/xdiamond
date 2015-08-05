a51350ff6f7b857daeff4c10df7c70d8bc326f21
hengyunabc <hengyunabc@gmail.com>
2015-08-05 16:06:33 +0800

    增加全局的异常处理

--- xdiamond-server/src/main/java/io/github/xdiamond/web/GlobalDefaultExceptionHandler.java
+++ xdiamond-server/src/main/java/io/github/xdiamond/web/GlobalDefaultExceptionHandler.java
@@ -1,14 +1,18 @@
 package io.github.xdiamond.web;
 
+import io.github.xdiamond.web.RestResult.RestResultBuilder;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authz.UnauthenticatedException;
 import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler {
@@ -33,6 +37,22 @@ public class GlobalDefaultExceptionHandler {
   public ResponseEntity<RestResult> loginFail() {
     return RestResult.fail().status(HttpServletResponse.SC_UNAUTHORIZED)
         .withErrorMessage("login fail").build();
+  }
+
+  @ExceptionHandler(Throwable.class)
+  @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+  @ResponseBody
+  public ResponseEntity<RestResult> internalError(Throwable throwable) {
+    RestResultBuilder restResultBuilder =
+        RestResult.fail().status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    if (throwable.getCause() != null) {
+      restResultBuilder.withErrorMessage(throwable.getCause().getMessage());
+    } else if (throwable.getMessage() != null) {
+      restResultBuilder.withErrorMessage(throwable.getMessage());
+    } else {
+      restResultBuilder.withErrorMessage(throwable.getClass().getName());
+    }
+    return restResultBuilder.build();
   }
 
 }
