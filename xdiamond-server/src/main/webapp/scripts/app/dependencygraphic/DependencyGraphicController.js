db4eaed5f8dc7da548e8060a0f96321f2368cae0
hengyunabc <hengyunabc@gmail.com>
2015-08-10 04:21:14 +0800

    完成依赖图显示功能，增加配置项

--- xdiamond-server/src/main/webapp/scripts/app/dependencygraphic/DependencyGraphicController.js
+++ xdiamond-server/src/main/webapp/scripts/app/dependencygraphic/DependencyGraphicController.js
@@ -7,110 +7,172 @@
 angular.module('xdiamondApp').controller("DependencyGraphicController",
     ['$scope', 'projects', 'dependencies', function ($scope, projects, dependencies) {
 
-        // 基于准备好的dom，初始化echarts图表
-        var myChart = echarts.init(document.getElementById('dependenciesechart'));
-
-        var nodes = [];
-        var links = [];
-
-        var rootNode = {
-            name: 'ROOT',
-            //value: 111,
-            // Custom properties
-            id: 0,
-            depth: 0,
-            category: 2,
-            ignore: true
+        var echartConfig = {
+            width: 800,
+            height: 800,
+            leftBorder: 80,
+            topBorder: 80,
+            bShowLabel: true,
+            bForceNodePos: true
         }
-        nodes.push(rootNode);
+        $scope.echartConfig = echartConfig;
 
-        projects.forEach(function (project) {
-            var node = {
-                name: project.artifactId + '|' + project.version,
-                //value: 0,
-                id: project.id,
-                depth: 0,
-                category: 0
+        var setChartOption = function () {
+            // 基于准备好的dom，初始化echarts图表
+            var parentElement = document.getElementById('parentChart');
+            while (parentElement.firstChild) {
+                parentElement.removeChild(parentElement.firstChild);
             }
-            nodes.push(node);
-        });
 
-        dependencies.forEach(function (dependency) {
-            var link = {
-                source: dependency.projectId,
-                target: dependency.dependencyProjectId,
-                weight: 1
+            var echartElement = document.createElement("div");
+            parentElement.appendChild(echartElement);
+
+            angular.element(echartElement).css('height', echartConfig.height + 'px').css('width', echartConfig.width + 'px');
+
+            var myChart = echarts.init(echartElement);
+
+            var nodes = [];
+            var links = [];
+
+            var projectIdToProject = {};
+            projects.forEach(function (project, index) {
+                projectIdToProject[project.id] = project;
+            })
+
+            var projectIdToNodeIndex = {};
+
+            var projectIdMaxLevelMap = {};
+
+            //对于每一个project，查找它的上一级的依赖，每找到一个level + 1，最终最大level就是这个project的level
+            var findMaxLevel = function (project) {
+                console.log("findMaxLevel, project.artifactId:" + project.artifactId);
+                var maxLevel = 0;
+                dependencies.forEach(function (dependency, index) {
+                    if (dependency.projectId === project.id) {
+                        if (maxLevel === 0) {
+                            maxLevel = 1;
+                        }
+                        var parentMaxLevel = findMaxLevel(projectIdToProject[dependency.dependencyProjectId]);
+                        if (parentMaxLevel + 1 > maxLevel) {
+                            maxLevel = parentMaxLevel + 1;
+                        }
+                    }
+                });
+                return maxLevel;
             };
-            links.push(link);
-        });
 
-
-        var option = {
-            title: {
-                text: '依赖图',
-                subtext: '项目之间的依赖关系',
-                x: 'right',
-                y: 'bottom'
-            },
-            tooltip: {
-                trigger: 'item',
-                formatter: '{b}'
-            },
-            toolbox: {
-                show: true,
-                feature: {
-                    restore: {show: true},
-                    magicType: {show: true, type: ['force', 'chord']},
-                    saveAsImage: {show: true}
+            //记录每一个level的node的总数
+            var levelNodeSizeMap = {}
+            var treeLevel = 0;
+            projects.forEach(function (project, index) {
+                var maxLevel = findMaxLevel(project);
+                if (maxLevel > treeLevel) {
+                    treeLevel = maxLevel;
                 }
-            },
-            legend: {
-                x: 'left',
-                data: ['叶子节点', '非叶子节点', '根节点']
-            },
-            series: [
-                {
-                    type: 'force',
-                    name: "依赖关系",
-                    ribbonType: false,
-                    categories: [
-                        {
-                            name: '叶子节点'
-                        },
-                        {
-                            name: '非叶子节点'
-                        },
-                        {
-                            name: '根节点'
+                projectIdMaxLevelMap[project.id] = maxLevel;
+                console.log('project artifactId:' + project.artifactId + ", maxLevel:" + maxLevel);
+                if (maxLevel in levelNodeSizeMap) {
+                    levelNodeSizeMap[maxLevel] = levelNodeSizeMap[maxLevel] + 1;
+                } else {
+                    levelNodeSizeMap[maxLevel] = 0;
+                }
+            })
+
+            // key是level, value是当前这一level已放了的node的数量
+            var currentLevelNodeSize = {};
+            projects.forEach(function (project, index) {
+                var projectMaxLevel = projectIdMaxLevelMap[project.id];
+                if (!currentLevelNodeSize[projectMaxLevel]) {
+                    currentLevelNodeSize[projectMaxLevel] = 0;
+                }
+
+                //计算出当前这一行的结点的平均距离，再计算出这个结点的具体X坐标
+                var x = echartConfig.leftBorder + (echartElement.clientWidth - echartConfig.leftBorder * 2) / levelNodeSizeMap[projectMaxLevel] * currentLevelNodeSize[projectMaxLevel];
+                currentLevelNodeSize[projectMaxLevel] = currentLevelNodeSize[projectMaxLevel] + 1;
+
+                var y = (echartElement.clientHeight - echartConfig.topBorder * 2) / treeLevel * projectMaxLevel + echartConfig.topBorder;
+
+                projectIdToNodeIndex[project.id] = index;
+                var node = {
+                    name: project.artifactId,
+                    initial: [x, y],
+                    fixY: echartConfig.bForceNodePos,
+                    fixX: echartConfig.bForceNodePos,
+                    project: project
+                }
+                nodes.push(node);
+            });
+
+            dependencies.forEach(function (dependency) {
+                var link = {
+                    source: projectIdToNodeIndex[dependency.dependencyProjectId],
+                    target: projectIdToNodeIndex[dependency.projectId],
+                    weight: 1,
+                    name: '依赖于'
+                };
+                links.push(link);
+            });
+
+            var option = {
+                title: {
+                    text: '依赖图',
+                    subtext: '项目之间的依赖关系',
+                    x: 'right',
+                    y: 'bottom'
+                },
+                tooltip: {
+                    trigger: 'item',
+                    formatter: function (params) {
+                        if (params.indicator2) {    // is edge
+                            return params.indicator2 + ' ' + params.name + ' ' + params.indicator;
+                        } else {    // is node
+                            return params.data.project.artifactId + '|' + params.data.project.version
                         }
-                    ],
-                    itemStyle: {
-                        normal: {
-                            label: {
-                                show: true,
-                                textStyle: {
-                                    color: '#333'
+                    }
+                },
+                toolbox: {
+                    show: true,
+                    feature: {
+                        restore: {show: true},
+                        magicType: {show: true, type: ['force', 'chord']},
+                        saveAsImage: {show: true}
+                    }
+                },
+                series: [
+                    {
+                        type: 'chord',
+                        sort: 'ascending',
+                        sortSub: 'descending',
+                        ribbonType: false,
+                        radius: '60%',
+                        itemStyle: {
+                            normal: {
+                                label: {
+                                    show: echartConfig.bShowLabel,
+                                    textStyle: {
+                                        color: '#333'
+                                    }
                                 }
-                            },
-                            nodeStyle: {
-                                brushType: 'both',
-                                borderColor: 'rgba(255,215,0,0.4)',
-                                borderWidth: 1
                             }
-                        }
-                    },
-                    minRadius: 10,
-                    maxRadius: 2,
-                    coolDown: 0.995,
-                    linkSymbol: 'arrow',
-                    nodes: nodes,
-                    links: links,
-                    steps: 1
-                }
-            ]
-        };
+                        },
+                        minRadius: 7,
+                        maxRadius: 20,
+                        linkSymbol: 'arrow',
+                        nodes: nodes,
+                        links: links,
+                        steps: 1
+                    }
+                ]
+            };
 
-        // 为echarts对象加载数据
-        myChart.setOption(option);
+            // 为echarts对象加载数据
+            myChart.setOption(option);
+        }
+
+        setChartOption();
+
+        $scope.changeDivConfig = function () {
+            setChartOption();
+        }
 
     }]);
